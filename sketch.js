5012995b6a68edc46e99c220f04180c88731a6f6
sunn2 <82132725+sunn2@users.noreply.github.com>
2021-05-06 02:28:43 +0900

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,117 @@
+let myOrange = []; 
+let myTokomon;
+let amount = 50;
+let img;
+
+function preload(){
+  img = loadImage('iloveorange.PNG');
+}
+
+function setup() {
+      createCanvas(windowWidth, windowHeight);
+      for (let i = 0; i < amount; i++){
+      myOrange[i] = new Orange(); 
+      }
+      myTokomon = new Tokomon();
+
+}
+
+function draw() {
+      background(250, 250, 200);
+
+      for (let i = 0; i < amount; i++){
+        myOrange[i].display();
+        myOrange[i].move();
+        // myOrange[i].eat();
+    }
+
+    myTokomon.display();
+    myTokomon.move();
+
+  }
+
+class Orange{ 
+
+    constructor(){ 
+        // this.c = [color(240, 170, 10), color(0,100,100)]; 
+        // this.colors = random(this.c);
+        this.c1 = color(240, 170, 10);
+        this.c2 = color(0, 100, 100);
+        this.xpos = random(width);
+        this.ypos = random(height);
+        this.xspeed = random(1, 5);
+        this.yspeed = random(1, 5);
+        this.r1 = 40;
+        this.r2 = 45;
+    }
+
+    display() { 
+      this.d = dist(mouseX, mouseY, this.xpos, this.ypos);
+      this.d2 = dist(mouseX, mouseY, this.xpos+50, this.ypos+50);
+        rectMode(CENTER);
+        noStroke();
+        fill(this.c1);
+        ellipse(this.xpos, this.ypos, this.r1);
+        fill(this.c2);
+        ellipse(this.xpos+50, this.ypos+50, this.r2);
+       
+        if(this.c1 && this.d < 35){
+          this.r1 = 0;
+        }
+        if(this.c2 && this.d2 < 35){
+          this.r2 += 10;
+        }   
+    }
+  
+    move() {
+      if (this.xpos >= width || this.xpos < 0){
+        this.xspeed = this.xspeed * -1;
+      }
+
+      if (this.ypos >= height || this.ypos < 0){
+        this.yspeed = this.yspeed * -1;
+      }
+      this.xpos = this.xpos + this.xspeed;
+      this.ypos = this.ypos + this.yspeed;
+    }
+
+    // eat(){
+    // this.d = dist(mouseX, mouseY, this.xpos, this.ypos);
+    // if(this.d < 70){
+    //   fill(250, 250, 200);
+    // }
+    //}
+  }
+
+class Tokomon {
+
+  constructor(){
+    this.tspeed = 10;
+    this.tx = 35;
+    this.ty = 35;
+  }
+
+  display(){
+    imageMode(CENTER);
+    image(img, this.tx, this.ty, 70, 70);
+    // this.tx = constrain(mouseX, 35, width-35);
+    // this.ty = constrain(mouseY, 35, height-35);
+  }
+
+  move(){
+    this.tx = constrain(mouseX, 35, width-35);
+    this.ty = constrain(mouseY, 35, height-35);
+  //   if(keyIsDown(UP_ARROW)){
+  //     this.ty = this.ty - this.tspeed;
+  //   }
+  //   if(keyIsDown(DOWN_ARROW)){
+  //     this.ty = this.ty + this.tspeed;
+  //   }
+  //   if(keyIsDown(LEFT_ARROW)){
+  //     this.tx = this.tx - this.tspeed;
+  //   }
+  //   if(keyIsDown(RIGHT_ARROW)){
+  //     this.tx = this.tx + this.tspeed;
+  //   }
+  }
+}
